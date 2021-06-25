6b3409d73673b4e91b87f48aa14b54b505d285a1
Charly Cimino <carlosecimino@gmail.com>
2021-06-25 11:27:40 -0300

    Listar detalle de métodos de una clase

--- src/reflection/Principal.java
+++ src/reflection/Principal.java
@@ -1,9 +1,7 @@
 package reflection;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 
 /**
  *
@@ -16,39 +14,50 @@ public class Principal {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         try {
-            // definimos el nombre de la clase (incluyendo el paquete)
+            // nombre de la clase
             String sClass = "java.lang.Object";
-            // obtengo una instancia de Class apuntando a la clase
             Class clazz = Class.forName(sClass);
-            Constructor ctor = clazz.getConstructor();
-            Object obj = ctor.newInstance();
-            
-            String mtdName = "toString";
-            System.out.println("Método " + mtdName);
-            Method mtd = obj.getClass().getDeclaredMethod(mtdName);
-
-            int i = mtd.getModifiers();
-
-            /*
-            Este método retorna un int dentro del cual se encuentra codificada en potencias
-            de 2 la lista de modificadores del método m. Para decodificar esta lista, tenemos que
-            usar las constantes definidas en la clase java.lang.reflect.Modifiers.
-            */
-            
-            // https://es.wikipedia.org/wiki/Operador_a_nivel_de_bits#Determinando_el_estado_de_bits
-            if ((i & Modifier.PUBLIC) == i) {
-                System.out.println("Es publico");
+            // lista de metodos
+            Method mtdos[] = clazz.getDeclaredMethods();
+            String m;
+            for (Method mtd : mtdos) {
+                m = "";
+                // determino qué modificadores aplican al metodo
+                m += _evalModif(mtd, Modifier.PRIVATE, "private");
+                m += _evalModif(mtd, Modifier.PROTECTED, "protected");
+                m += _evalModif(mtd, Modifier.PUBLIC, "public");
+                m += _evalModif(mtd, Modifier.STATIC, "static");
+                m += _evalModif(mtd, Modifier.ABSTRACT, "abstract");
+                m += _evalModif(mtd, Modifier.FINAL, "final");
+                m += _evalModif(mtd, Modifier.SYNCHRONIZED, "synchronized");
+                // determino que tipo de datos retorna
+                m += " " + mtd.getReturnType().getSimpleName() + " ";
+                // determino el nombre del metodo
+                m += mtd.getName();
+                // obtengo la lista de parametros
+                m += _tiposParams(mtd.getParameterTypes());
+                // muestro el prototipo del metodo
+                System.out.println(m);
             }
-            if ((i & Modifier.STATIC) == i) {
-                System.out.println("Es estatico");
-            }
-            
-            Class clazzReturn = mtd.getReturnType();
-            System.out.println( "Retorna un " + clazzReturn.getName() );
 
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private static String _tiposParams(Class[] parameterTypes) {
+        String ret = "";
+        ret += "(";
+        for (int i = 0; i < parameterTypes.length; i++) {
+            ret += parameterTypes[i].getSimpleName()
+                    + ((i < parameterTypes.length - 1) ? "," : "");
+        }
+        ret += ")";
+        return ret;
+    }
+
+    private static String _evalModif(Method mtd, int m, String desc) {
+        return (mtd.getModifiers() & m ) == m ? " " + desc : "";
     }
 
 }
