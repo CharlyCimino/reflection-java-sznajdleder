302eefacbc47a6ab71afaf81f1e4f1f19d4f7a27
Charly Cimino <carlosecimino@gmail.com>
2021-06-25 10:56:06 -0300

    Frame creado dinámicamente

--- src/reflection/Principal.java
+++ src/reflection/Principal.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 /**
@@ -9,19 +10,31 @@ import java.lang.reflect.Method;
  * GitHub: https://github.com/CharlyCimino
  */
 public class Principal {
-
+    
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         try {
             // definimos el nombre de la clase (incluyendo el paquete)
-            String sClass = "reflection.Persona";
+            String sClass = "java.awt.Frame";
             // obtengo una instancia de Class apuntando a la clase
             Class clazz = Class.forName(sClass);
-            // obtengo la lista de methods (de tipo Method) de la clase
-            Method metodos[] = clazz.getDeclaredMethods();
-            // recorro la lista de methods y muestro sus nombres
-            for (Method mtd : metodos) {
-                System.out.println(mtd.getName());
-            }
+            // invoco al constructor que recibe un String como argumento
+            Class[] paramTypesConstructor = {String.class};
+            Object[] paramValuesConstructor = {"Ventana AWT Reflect"};
+            Constructor ctor = clazz.getConstructor(paramTypesConstructor);
+            Object obj = ctor.newInstance(paramValuesConstructor);
+            // invoco al metodo setSize que recibe dos enteros
+            String mtdName1 = "setSize";
+            Class[] pTypes1 = {Integer.TYPE, Integer.TYPE};
+            Object[] pValues1 = {300, 300};
+            Method mtd = obj.getClass().getMethod(mtdName1, pTypes1);
+            mtd.invoke(obj, pValues1);
+            // invoco al metodo setVisible que recibe un boolean
+            String mtdName2 = "setVisible";
+            Class[] pTypes2 = {Boolean.TYPE};
+            Object[] pValues2 = {true};
+            Method mtd2 = obj.getClass().getMethod(mtdName2, pTypes2);
+            mtd2.invoke(obj, pValues2);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
